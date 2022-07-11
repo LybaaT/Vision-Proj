33ca1696f8557510a811e3ffbfa6b64eb8796705
LybaaT <105508273+LybaaT@users.noreply.github.com>
2022-07-12 03:07:38 +0500

    updated

--- calc.js
+++ calc.js
@@ -1,16 +1,18 @@
-function insert(num) {
-    document.forms[0][0].value = document.forms[0][0].value + num
-}
-function equal() {
-    var exp = document.forms[0][0].value;
-    if(exp) {
-        document.forms[0][0].value = eval(exp)
+
+    function insert(num) {
+        document.forms[0][0].value = document.forms[0][0].value + num
     }
-}
-function clean() {
-    document.forms[0][0].value = "";
-}
-function back() {
-    var exp = document.forms[0][0].value;
-    document.forms[0][0].value = exp.substring(0, exp.length -1)
-}
+    function equal() {
+        var exp = document.forms[0][0].value;
+        if(exp) {
+            document.forms[0][0].value = eval(exp)
+        }
+    }
+    function clean() {
+        document.forms[0][0].value = "";
+    }
+    function back() {
+        var exp = document.forms[0][0].value;
+        document.forms[0][0].value = exp.substring(0, exp.length -1)
+    }
+
