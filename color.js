2619851ac36536cf1c9303f19a52f9e116fca8cc
LybaaT <105508273+LybaaT@users.noreply.github.com>
2022-07-12 03:26:07 +0500

    changed h1

--- color.js
+++ color.js
@@ -1,7 +1,7 @@
 const setBg1 = () => {
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     document.getElementById('color1').style.backgroundColor = "#" + randomColor;
-    color1.innerHTML = "#" + randomColor;
+    color1.innerHTML = "<h1>#" + randomColor + "</h1>";
     }
     genNew.addEventListener("click", setBg1);
     setBg1();
@@ -9,7 +9,7 @@ const setBg1 = () => {
 const setBg2 = () => {
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     document.getElementById('color2').style.backgroundColor = "#" + randomColor;
-    color2.innerHTML = "#" + randomColor;
+    color2.innerHTML = "<h1>#" + randomColor + "</h1>";
     }
     genNew.addEventListener("click", setBg2);
     setBg2();
@@ -17,7 +17,7 @@ const setBg2 = () => {
 const setBg3 = () => {
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     document.getElementById('color3').style.backgroundColor = "#" + randomColor;
-    color3.innerHTML = "#" + randomColor;
+    color3.innerHTML = "<h1>#" + randomColor + "</h1>";
     }
     genNew.addEventListener("click", setBg3);
     setBg3();
@@ -25,7 +25,7 @@ const setBg3 = () => {
 const setBg4 = () => {
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     document.getElementById('color4').style.backgroundColor = "#" + randomColor;
-    color4.innerHTML = "#" + randomColor;
+    color4.innerHTML = "<h1>#" + randomColor + "</h1>";
     }
 
     genNew.addEventListener("click", setBg4);
@@ -34,7 +34,7 @@ const setBg4 = () => {
 const setBg5 = () => {
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     document.getElementById('color5').style.backgroundColor = "#" + randomColor;
-    color5.innerHTML = "#" + randomColor;
+    color5.innerHTML = "<h1>#" + randomColor + "</h1>";
     }
     
     genNew.addEventListener("click", setBg5);
