df9697f7dcdc30bfdd12b26a3329a42661f68fca
LybaaT <105508273+LybaaT@users.noreply.github.com>
2022-07-12 17:15:30 +0500

    made buttons typeable

--- calc.js
+++ calc.js
@@ -1,4 +1,16 @@
+function keyPress(event) {
+    console.log(event);
+    if (['1','2','3', '4', '5', '6', '7', '8', '9', '0', '.', '+', '-', '/', '*', '='].includes(event.key)) {
+        insert(event.key);
+        } 
+        else {
+        console.log(event.key + ' was not added');
+        };    
+    }
+  
+  document.body.onkeydown = keyPress;
 
+    
     function insert(num) {
         document.forms[0][0].value = document.forms[0][0].value + num
     }
