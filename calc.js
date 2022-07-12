41d4bfbe39adf2c6d42139c026c7a1d6f1ce684b
LybaaT <105508273+LybaaT@users.noreply.github.com>
2022-07-12 17:28:46 +0500

    fixed calc

--- calc.js
+++ calc.js
@@ -1,4 +1,5 @@
 function keyPress(event) {
+    console.log();
     console.log(event);
     if (['1','2','3', '4', '5', '6', '7', '8', '9', '0', '.', '+', '-', '/', '*'].includes(event.key)) {
         insert(event.key);
