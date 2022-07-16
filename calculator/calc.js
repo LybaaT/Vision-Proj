c458ad0e74b6f1a57f61adb12c78de11c9234526
Lyba T <105508273+LybaaT@users.noreply.github.com>
2022-07-16 18:44:03 +0500

    updatez

--- calculator/calc.js
+++ calculator/calc.js
@@ -7,28 +7,33 @@ function keyPress(event) {
     else if(['Enter'].includes(event.key)) {
         equal();
     }
-        else {
+    else if(['Backspace'].includes(event.key)) {
+        back();
+    }
+    else if(['Delete'].includes(event.key)) {
+        clean();
+    }
+    else {
         console.log(event.key + ' was not added');
-        };    
-    }
-  
-  document.body.onkeydown = keyPress;
+    };    
+}
+ 
+document.body.onkeydown = keyPress;
 
-    
-    function insert(num) {
-        document.forms[0][0].value = document.forms[0][0].value + num
+function insert(num) {
+    document.forms[0][0].value = document.forms[0][0].value + num
+}
+function equal() {
+    var exp = document.forms[0][0].value;
+    if(exp) {
+        document.forms[0][0].value = eval(exp)
     }
-    function equal() {
-        var exp = document.forms[0][0].value;
-        if(exp) {
-            document.forms[0][0].value = eval(exp)
-        }
-    }
-    function clean() {
-        document.forms[0][0].value = "";
-    }
-    function back() {
-        var exp = document.forms[0][0].value;
-        document.forms[0][0].value = exp.substring(0, exp.length -1)
-    }
+}
+function clean() {
+    document.forms[0][0].value = "";
+}
+function back() {
+    var exp = document.forms[0][0].value;
+    document.forms[0][0].value = exp.substring(0, exp.length -1)
+}
 
