5a611a692bf2853391b8a228ca1f0eb2eafc407c
WarzeEgg <contact@warze.org>
2022-07-17 14:27:00 +0200

    Made navbar unified + other fixes

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,12 +1,35 @@
-let navbarshown = true;
+// Creating the navmenu element
+
+const navmenu = document.createElement('div');
+navmenu.classList.add('navmenu');
+
+// All of the links across all pages
+
+navmenu.innerHTML = `
+<a href="calculator.html">Calculator</a>
+<a href="colorgenerator.html">Color Generator</a>
+<a href="stopwatch.html">Stopwatch</a>
+<a href="timer.html">Timer</a>`;
+
+navmenu.style.transform = 'translateX(-300px)';
+navmenu.style.transition = 'transform 0.2s ease-out';
+
+// The state of whether the navbar is current visible
+
+let navbarshown = false;
+
+// The function that is called when clicking the logo
+
 function openNavBar() {
     navbarshown = !navbarshown;
-
+    
     const transform = (navbarshown ? 'translateX(0px)' : 'translateX(-300px)');
-    document.querySelector('.navmenu').style.transform = transform;
+    navmenu.style.transform = transform;
 };
-openNavBar();
+const logo = document.querySelector('.logo');
+logo.onclick = openNavBar;
 
-setTimeout(function() {
-    document.querySelector('.navmenu').style.transition = 'transform 0.2s ease-out';
-}, 1);
+// Insert the navmenu after the nav element
+
+const nav = document.querySelector('.nav');
+nav.after(navmenu);
