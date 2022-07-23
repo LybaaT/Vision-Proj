965365cd95c19553a9a814988a2f1a3440b854fb
WarzeEgg <contact@warze.org>
2022-07-23 18:41:38 +0200

    Fixed the color generator
    - The fact that 5 digit hex codes could be generated
    - The button didn't work on the direct page

--- colorgenerator/index.js
+++ colorgenerator/index.js
@@ -1,6 +1,7 @@
 function generateNewColors() {
     for (let i = 1; i <= 5; i ++) {
-        const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16).padStart('0', 6);
+        const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
+        console.log(randomColor);
         const element = document.getElementById('color' + i);
         element.style.backgroundColor = randomColor;
         element.innerHTML = `<h1>${randomColor}</h1>`;
@@ -8,5 +9,4 @@ function generateNewColors() {
 };
 
 const genNew = document.getElementById('genNew');
-genNew.onclick = generateNewColors;
 generateNewColors();
